00d5c7a9b3998fec11fc835ad1fd56cd4d95b1a3
kanalfred <kanalfred@gmail.com>
2017-11-05 01:14:06 -0800

    Create backupDB.pl

---
+++ sql/backupDB.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+############ Config Vars #############################
+
+$mysqluser="admin";  # Mysql user with root access
+
+#$mysqlpassword="w104\@r4t0\!";  # Mysql user password
+$mysqlpassword="xxxxx";  # Mysql user password
+
+@exclude=('test','information_schema','mysql','mysql.event'); # exclude these databases ('db_name', 'db2_name', ...)
+
+$backupdir="/var/backup/mysql";   # Directory where backup files will be saved exclude trailing slash (/)
+
+$mysqlshow ="/usr/bin/mysqlshow"; # Location of the mysqlshow command
+
+$mysqldump ="/usr/bin/mysqldump"; # Location of the mysqldump command
+
+########## Do not edit below this line ###########################
+
+
+@databases= `$mysqlshow -u $mysqluser --password=$mysqlpassword`;
+
+$i=0;
+
+foreach $db (@databases){
+	if($i>2 && $db!~/^\+/){	
+	$db=~s/\s//g;
+        $db=~s/\|//g;	
+		foreach (@exclude){
+			if($db eq $_){
+ 				splice(@databases, $i, 1);
+			}	
+		}
+        }
+	$i++;	
+}
+$i=0;
+foreach $db (@databases){
+        if($i>2 && $db!~/^\+/){
+                $db=~s/\s//g;
+                $db=~s/\|//g;
+               # print "$mysqldump -u $mysqluser --password=$mysqlpassword $db > $backupdir/$db.sql \n";
+                `$mysqldump -u $mysqluser --password=$mysqlpassword --events $db > $backupdir/$db.sql`;
+               
+        }
+        $i++;
+}
