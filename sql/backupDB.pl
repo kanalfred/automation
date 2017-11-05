2dc15c42f6bc38f58435b2dc5b692f9b7d97e169
kanalfred <kanalfred@gmail.com>
2017-11-05 02:14:31 -0800

    Update backupDB.pl

--- sql/backupDB.pl
+++ sql/backupDB.pl
@@ -8,8 +8,9 @@ $mysqluser="admin";  # Mysql user with root access
 
 #$mysqlpassword="w104\@r4t0\!";  # Mysql user password
 $mysqlpassword="xxxxx";  # Mysql user password
+$mysqlhost="192.168.3.112";
 
-@exclude=('test','information_schema','mysql','mysql.event'); # exclude these databases ('db_name', 'db2_name', ...)
+@exclude=('test','information_schema','mysql','mysql.event','performance_schema'); # exclude these databases ('db_name', 'db2_name', ...)
 
 $backupdir="/var/backup/mysql";   # Directory where backup files will be saved exclude trailing slash (/)
 
@@ -20,7 +21,7 @@ $mysqldump ="/usr/bin/mysqldump"; # Location of the mysqldump command
 ########## Do not edit below this line ###########################
 
 
-@databases= `$mysqlshow -u $mysqluser --password=$mysqlpassword`;
+@databases= `$mysqlshow -u $mysqluser --password=$mysqlpassword -h $mysqlhost`;
 
 $i=0;
 
@@ -42,7 +43,7 @@ foreach $db (@databases){
                 $db=~s/\s//g;
                 $db=~s/\|//g;
                # print "$mysqldump -u $mysqluser --password=$mysqlpassword $db > $backupdir/$db.sql \n";
-                `$mysqldump -u $mysqluser --password=$mysqlpassword --events $db > $backupdir/$db.sql`;
+                `$mysqldump -u $mysqluser --password=$mysqlpassword -h $mysqlhost --skip-lock-table --events $db > $backupdir/$db.sql`;
                
         }
         $i++;
